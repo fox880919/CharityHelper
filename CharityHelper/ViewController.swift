6448eba27a3a3fd4e11ba4db7c0aa15cc638a269
Faiez Altamimi <the-fox-88@live.com>
2017-08-21 17:02:17 +0100

    Initial Commit

---
+++ CharityHelper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CharityHelper
+//
+//  Created by Fayez Altamimi on 21/08/2017.
+//  Copyright © 2017 Fayez Altamimi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
